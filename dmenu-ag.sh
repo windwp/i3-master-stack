0760adc390500a7abdfe44e8d651e2e7712398b1
windwp <baronnight19@gmail.com>
2021-07-24 18:35:22 +0700

    make dmenu center

--- dmenu-ag.sh
+++ dmenu-ag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # use dmenu to excute command because perfomance of rofi
 # use need to patch dmenu with center and border with patch
-dmenuCommand="dmenu -i -bw 2 -l 10 -fn 'Hack' -p '' "
+dmenuCommand="dmenu -i -bw 2 -l 10 -fn 'Hack' -p '' -c "
 # dmenuCommand="dmenu -i -l 10 -fn 'Hack' -p '✟' "
 command="./rofi-ag.sh | ${dmenuCommand}" 
 status="2" 
